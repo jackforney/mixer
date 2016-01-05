4abcd6364082ad60172894f4b9bed4b66bec021e
jackforney <jack.forney@yahoo.com>
2016-01-04 21:14:02 -0800

    Introduce kv file
    Use kv language. Create functioning buttons (still with pseudo output).

--- main.py
+++ main.py
@@ -1,53 +1,27 @@
-from time import sleep
 from kivy.app import App
-#kivy.require("1.8.0")
+from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
-#from kivy.uix.widget import Widget
-from kivy.uix.button import Button
+from time import sleep
 
+class SelectionScreen(GridLayout):
+    def makeGT(self):
+        print ("1 part Gin")
+        sleep(1)
+        print ("3 parts Tonic Water")
+        sleep(3)
+        print ("All done!")
+        return True
 
-def makeGinTonic():
-    print ('1 part Gin')
-    sleep(1)
-    print ('3 parts Tonic')
-    sleep(3)
-    print ('Drink is ready')
-    return
-
-
-def makeWhiskeyGinger():
-    print ('1 part Whiskey')
-    sleep(1)
-    print ('3 parts Ginger Ale')
-    sleep(3)
-    print ('Drink is ready')
-    return
-
-
-def makeWhiskeyTriple():
-    print ('1 part Gin')
-    sleep(1)
-    print ('3 parts Tonic')
-    sleep(3)
-    print ('Drink is ready')
-    return
-
-
-class SelectScreen(GridLayout):
-    def __init__(self, **kwargs):
-        super(SelectScreen, self).__init__(**kwargs)
-        self.cols = 3
-        self.padding = 25
-        self.spacing = 25
-        self.add_widget(Button(text="Gin & Tonic"))
-        self.add_widget(Button(text="Whiskey Ginger"))
-        self.add_widget(Button(text="Whiskey Triple"))
-
-
-class MixerApp(App):
+    def makeWG(self):
+        print ("1 part Whiskey")
+        sleep(1)
+        print ("3 parts Ginger Ale")
+        sleep(3)
+        print ("All done!")
+        
+class DrinkMixer(App):
     def build(self):
-        return SelectScreen()
-
-
-if __name__ == "__main__":
-    MixerApp().run()
+        return SelectionScreen()
+    
+if __name__== "__main__":
+    DrinkMixer().run()
